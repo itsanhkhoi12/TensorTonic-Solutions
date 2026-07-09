e954c460e1fa1128302b61b4fbe85193352be28a
Anh Khoi <anhkhoi5602@gmail.com>
2026-07-09 21:45:39 +0700

    Solve precision-recall-at-k

---
+++ precision-recall-at-k/precision-recall-at-k.py
@@ -0,0 +1,6 @@
+def precision_recall_at_k(recommended, relevant, k):
+    """
+    Compute precision@k and recall@k for a recommendation list.
+    """
+    # Write code here
+    return [(len(set(recommended[:k]) & set(relevant))) / k, (len(set(recommended[:k]) & set(relevant))) / len(relevant)]
