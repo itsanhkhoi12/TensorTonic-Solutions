fb2bc8ab717b00ce6ebae640ca88ebfa46865ca2
Anh Khoi <anhkhoi5602@gmail.com>
2026-08-02 00:46:43 +0700

    Solve exponential-moving-average

---
+++ exponential-moving-average/exponential-moving-average.py
@@ -0,0 +1,14 @@
+def exponential_moving_average(values, alpha):
+    """
+    Compute the exponential moving average of the given values.
+    """
+    # Write code here
+
+    ema = []
+    ema.append(values[0])
+
+    for i in range(1,len(values)):
+        ema.append(alpha*values[i]+(1-alpha)*ema[i-1])
+
+    return ema
+    
